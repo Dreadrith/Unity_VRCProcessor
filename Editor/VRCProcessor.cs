1dde0db33ef7fdfd45f947e574f4425c881370f2
Fynn9563 <mobo9563@gmail.com>
2024-05-02 15:03:11 +1000

    Added files

---
+++ Editor/VRCProcessor.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+//This work is licensed under the Creative Commons Attribution-NonCommercial 2.0 License.
+//To view a copy of the license, visit https://creativecommons.org/licenses/by-nc/2.0/legalcode
+
+//Made by Dreadrith#3238
+//Discord: https://discord.gg/ZsPfrGn
+//Github: https://github.com/Dreadrith/DreadScripts
+//Gumroad: https://gumroad.com/dreadrith
+//Ko-fi: https://ko-fi.com/dreadrith
+
+namespace DreadScripts.VRCProcessor
+{
+    public class VRCProcessor : AssetPostprocessor
+    {
+        private static ModelImporter importer;
+        private static bool willProcess;
+        private static bool isHuman;
+        private void OnPreprocessModel()
+        {
+            importer = assetImporter as ModelImporter;
+            willProcess = isHuman = false;
+            string guid = AssetDatabase.AssetPathToGUID(importer.assetPath);
+            if (importer && !SessionState.GetBool(guid, false))
+            {
+                SessionState.SetBool(guid, true);
+                willProcess = true;
+                importer.isReadable = true;
+                importer.importBlendShapeNormals = ModelImporterNormals.Import;
+            }
+        }
+
+        private void OnPostprocessMeshHierarchy(GameObject g)
+        {
+            if (willProcess && !isHuman)
+            {
+
+                Transform[] allTransforms = g.GetComponentsInChildren<Transform>();
+                if (allTransforms.Length < 15) return;
+
+                int hits = allTransforms.Count(t => t.name.IndexOf("Armature", System.StringComparison.OrdinalIgnoreCase) >= 0
+                                                    || t.name.IndexOf("Hand", System.StringComparison.OrdinalIgnoreCase) >= 0
+                                                    || t.name.IndexOf("Head", System.StringComparison.OrdinalIgnoreCase) >= 0);
+
+                if (hits >= 2)
+                {
+                    importer.animationType = ModelImporterAnimationType.Human;
+                    isHuman = true;
+                }
+            }
+        }
+
+        private void OnPostprocessModel(GameObject g)
+        {
+            if (willProcess && isHuman)
+            {
+                var description = importer.humanDescription;
+                var human = description.human.ToList();
+                for (int i = human.Count - 1; i >= 0; i--)
+                {
+                    switch (human[i].humanName)
+                    {
+                        case "Jaw" when human[i].boneName.IndexOf("jaw", System.StringComparison.OrdinalIgnoreCase) < 0:
+                        case "Upper Chest":
+                            human.RemoveAt(i);
+                            break;
+                    }
+                }
+
+                description.human = human.ToArray();
+                importer.humanDescription = description;
+            }
+        }
+
+        private static void OnPostprocessAllAssets(string[] ignored, string[] deletedAssets, string[] ignored2, string[] ignored3)
+        {
+            foreach (string s in deletedAssets)
+                SessionState.EraseBool(AssetDatabase.AssetPathToGUID(s));
+        }
+
+
+        [UnityEditor.Callbacks.DidReloadScripts]
+        private static void CollectExistingModels()
+        {
+            foreach (var g in AssetDatabase.FindAssets("t:model"))
+                SessionState.SetBool(g, true);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Texture Utility
+Automatically sets valid models to Humanoid and other usual settings upon import.
+
+## Features
+- Sets Normals to import
+- Enables Read/Write
+- Sets Humanoid Rig for Humanoid models
+- Removes Upper Chest bone
+- Removes Jaw bone if invalid
+
+### Thank you
+If you enjoy Texture Utility, please consider [supporting me ♡](https://ko-fi.com/Dreadrith)!
